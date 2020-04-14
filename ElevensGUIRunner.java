4b2a2a7735ee51d50d8e311658d113616c77cf97
Aaron Kiedes <akiedes@vtsd.com>
2020-04-14 11:43:27 -0400

    Starter Files

--- ElevensGUIRunner.java
+++ ElevensGUIRunner.java
@@ -3,23 +3,23 @@
  * See accompanying documents for a description of how Elevens is played.
  ************************************************************************
  * This is the original AP Elevens Lab Java program code.
- * 03-26-15 slightly altered by Leon Schram 
+ * 03-26-15 slightly altered by Leon Schram
  * who likes curly braces aligned.
  ************************************************************************
  * DO NOT ALTER THIS CLASS FOR LAB16!!!!!
  */
-public class ElevensGUIRunner 
+public class ElevensGUIRunner
 {
 
 	/**
 	 * Plays the GUI version of Elevens.
 	 * @param args is not used.
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args)
    {
 		ElevensBoard board = new ElevensBoard();
 		CardGameGUI gui = new CardGameGUI(board);
 		gui.displayGame();
 	}
-   
+
 }
